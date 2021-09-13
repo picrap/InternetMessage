0b8074833f9473f09b33294864abe35356bef62a
Pascal Craponne <pascal.craponne@arx.one>
2021-09-13 15:54:14 +0200

    Added encoding

---
+++ InternetMessage/Utility/Encoding.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+namespace InternetMessage.Utility
+{
+    public static class Encoding
+    {
+        public static string Decode(this string s)
+        {
+            if (!s.StartsWith("=?") || !s.EndsWith("?="))
+                return s;
+            // format is “=?«charset»?«encoding»?«encoded-text»?=”
+            var parts = s.Split('?');
+            return parts[2] switch
+            {
+                "B" => DecodeBase64(parts[1], parts[3]),
+                "Q" => DecodeQuotedPrintable(parts[1], parts[3]),
+                _ => throw new InvalidOperationException()
+            };
+        }
+
+        private static string DecodeQuotedPrintable(string charset, string encodedText)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string DecodeBase64(string charset, string encodedText)
+        {
+            var bytes = Convert.FromBase64String(encodedText);
+            var encoding = System.Text.Encoding.GetEncoding(charset);
+            return encoding.GetString(bytes);
+        }
+    }
+}
---
+++ InternetMessageTest/EncodingTest.cs
@@ -0,0 +1,25 @@
+﻿using InternetMessage.Utility;
+using NUnit.Framework;
+
+namespace InternetMessageTest
+{
+    [TestFixture]
+    public class EncodingTest
+    {
+        [Test]
+        public void NotEncodedTest()
+        {
+            var e = "Not encoded";
+            var s = e.Decode();
+            Assert.AreEqual(e, s);
+        }
+
+        [Test]
+        public void Base64EncodedTest()
+        {
+            var e = "=?utf-8?B?U2ltcGxlLnR4dA==?=";
+            var s = e.Decode();
+            Assert.AreEqual("Simple.txt", s);
+        }
+    }
+}
