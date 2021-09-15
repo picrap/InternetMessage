c288599e533709c974f30fd350dfa7d14dd13ea2
Pascal Craponne <pascal@craponne.fr>
2021-09-15 23:35:52 +0200

    Implemented quoted-printable decoder

--- InternetMessage/Encoding/QuotedPrintableDecoder.cs
+++ InternetMessage/Encoding/QuotedPrintableDecoder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace InternetMessage.Encoding
 {
@@ -9,8 +10,52 @@ namespace InternetMessage.Encoding
 
         public override byte[] Decode(string encodedString)
         {
-            // yeah, some day
-            throw new NotImplementedException();
+            var bytes = new List<byte>();
+            for (int i = 0; i < encodedString.Length; i++)
+            {
+                var c = encodedString[i];
+                if (c == '=')
+                {
+                    if (i == encodedString.Length - 1)
+                        throw new FormatException("Encoded string ends with an = sign");
+                    var c1 = encodedString[i + 1];
+                    if (IsCrLf(c1))
+                    {
+                        if (i == encodedString.Length - 2)
+                        {
+                            i++;
+                        }
+                        else
+                        {
+                            var c2 = encodedString[i + 1];
+                            if (!IsCrLf(c2))
+                                i++;
+                            else
+                                i += 2;
+                        }
+                    }
+                    else if (IsHex(c1))
+                    {
+                        if (i == encodedString.Length - 2)
+                            throw new FormatException($"Incomplete hexadecimal sequence at end");
+                        var c2 = encodedString[i + 2];
+                        if (!IsHex(c2))
+                            throw new FormatException($"Invalid character '{c2}' after = sign at {i + 2}");
+                        bytes.Add(Convert.ToByte(new string(new[] { c1, c2 }), 16));
+                        i += 2;
+                    }
+                    else
+                        throw new FormatException($"Invalid character '{c1}' after = sign at {i + 1}");
+                }
+                else
+                {
+                    bytes.Add((byte)c);
+                }
+            }
+            return bytes.ToArray();
         }
+
+        private static bool IsHex(char c) => c >= '0' && c <= '9' || c >= 'A' && c <= 'F';
+        private static bool IsCrLf(char c) => c == '\n' || c == '\r';
     }
 }
--- InternetMessageTest/EncodingTest.cs
+++ InternetMessageTest/EncodingTest.cs
@@ -1,4 +1,7 @@
-﻿using InternetMessage.Utility;
+﻿using System;
+using System.Net.Mime;
+using InternetMessage.Encoding;
+using InternetMessage.Utility;
 using NUnit.Framework;
 
 namespace InternetMessageTest
@@ -20,6 +23,53 @@ namespace InternetMessageTest
             var e = "=?utf-8?B?U2ltcGxlLnR4dA==?=";
             var s = e.Decode();
             Assert.AreEqual("Simple.txt", s);
+        }
+
+        [Test]
+        public void QuotedPrintableEncodedTest()
+        {
+            var e = "a=40b";
+            var qp = new QuotedPrintableDecoder();
+            var s = System.Text.Encoding.Default.GetString(qp.Decode(e));
+            Assert.AreEqual("a@b", s);
+        }
+
+        [Test]
+        public void QuotedPrintableCrLfEncodedTest()
+        {
+            var e = "a=\r\nb";
+            var qp = new QuotedPrintableDecoder();
+            var s = System.Text.Encoding.Default.GetString(qp.Decode(e));
+            Assert.AreEqual("ab", s);
+        }
+
+        [Test]
+        public void QuotedPrintableCrEncodedTest()
+        {
+            var e = "a=\rb";
+            var qp = new QuotedPrintableDecoder();
+            var s = System.Text.Encoding.Default.GetString(qp.Decode(e));
+            Assert.AreEqual("ab", s);
+        }
+
+        [Test]
+        public void QuotedPrintableKeepCrLfEncodedTest()
+        {
+            var e = "a\r\nb";
+            var qp = new QuotedPrintableDecoder();
+            var s = System.Text.Encoding.Default.GetString(qp.Decode(e));
+            Assert.AreEqual("a\r\nb", s);
+        }
+
+        [Test]
+        [TestCase("a=")]
+        [TestCase("a=1")]
+        [TestCase("a=0Z")]
+        [TestCase("a=X")]
+        public void InvalidQuotedPrintableEncodedTest(string s)
+        {
+            var qp = new QuotedPrintableDecoder();
+            Assert.Throws<FormatException>(() => qp.Decode(s));
         }
     }
 }
